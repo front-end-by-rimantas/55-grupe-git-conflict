b0dc1b2aba004de08368dacbe2e0c69928dd6eab
Rimantas <belauzas@gmail.com>
2025-05-29 09:25:34 +0300

    Contact

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { header } from './conponents/header.js';
+import { contact } from './conponents/contact.js';
 import { footer } from './conponents/footer.js';
 
 header();
+contact();
 footer();
